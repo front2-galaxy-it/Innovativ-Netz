3baa4d06d2b015e79f7dec67908748743891e147
Galaxy Develope <94291571+Boskolife@users.noreply.github.com>
2025-02-04 09:02:58 +0200

    add main page

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+burgerInit();
+// findHref();
+
+function burgerInit() {
+  const burger = document.querySelector('.header_burger');
+  const menu = document.querySelector('.nav_wrap');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('burger_active');
+    menu.classList.toggle('menu_active');
+    document.body.classList.toggle('body_lock');
+  });
+}
+
+function findHref() {
+  const menu = document.getElementById('menu');
+
+  if (menu) {
+    const links = menu.querySelectorAll('a');
+    const url = window.location.href;
+
+    links.forEach((link) => {
+      if (link.href && url === link.href) {
+        link.classList.add('menu_link_active');
+      } else {
+        link.classList.remove('menu_link_active');
+      }
+    });
+  }
+}
